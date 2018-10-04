dcaaa78d60323591c1681077e27ae63b56087b85
장우석 <cyj2651@naver.com>
2018-10-04 17:16:47 +0900

    drill_8 랜덤이동
    캐릭터 이동 가능
    값은 랜덤

--- Drill_08/drill_8_이동.py
+++ Drill_08/drill_8_이동.py
@@ -41,6 +41,41 @@ def draw_line(p1, p2):
         delay(0.02)
     pass
 
+def draw_curve_4_points(p1, p2, p3, p4):
+
+    # draw p1-p2
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+        DrawCharacter(x, y)
+        delay(0.02)
+
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        DrawCharacter(x, y)
+        delay(0.02)
+
+    # draw p3-p4
+    for i in range(50, 100, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
+        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+        DrawCharacter(x, y)
+        delay(0.02)
+
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p3[0] + (3*t**3 - 5*t**2 + 2)*p4[0] + (-3*t**3 + 4*t**2 + t)*p1[0] + (t**3 - t**2)*p2[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p3[1] + (3*t**3 - 5*t**2 + 2)*p4[1] + (-3*t**3 + 4*t**2 + t)*p1[1] + (t**3 - t**2)*p2[1])/2
+
+
+        DrawCharacter(x, y)
+        delay(0.02)
 
 
 
@@ -51,8 +86,9 @@ points = [(random.randint(0, KPU_WIDTH/2), random.randint(0, KPU_HEIGHT/2)) for 
 n = 1
 
 while True:
+    draw_curve_4_points(points[n], points[n+1],points[n+2], points[n+3])
 
-    draw_line(points[n-1], points[n])
+
     n = (n + 1) % size
 
 close_canvas()
