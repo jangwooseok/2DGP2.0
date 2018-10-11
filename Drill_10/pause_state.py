038665e5ddd4f462591a7bc0c56cddba7ccd020a
장우석 <cyj2651@naver.com>
2018-10-11 20:55:04 +0900

    drill 10 - 2 완료
    2015182036 장우석
    커밋 4회
    10-1 : https://youtu.be/qf8xypHqUcs
    10-2 : https://youtu.be/smcIcNyTRA0

--- Drill_10/pause_state.py
+++ Drill_10/pause_state.py
@@ -7,7 +7,7 @@ import main_state
 name = "PauseState"
 image = None
 logo_time = 0.0
-
+draw_time = 0
 
 def enter():
     global image
@@ -35,9 +35,17 @@ def update():
 
 def draw():
     global image
+    global draw_time
     clear_canvas()
-    main_state.draw()
-    image.draw(400, 300)
+    main_state.boy.draw()
+    main_state.grass.draw()
+    if draw_time > 0.10:
+        draw_time = 0
+    elif draw_time <= 0.05:
+        image.draw(400, 300)
+    delay(0.05)
+    draw_time += 0.01
+
     update_canvas()
 
     pass
@@ -48,7 +56,9 @@ def draw():
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN and event.key == SDLK_p:
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.pop_state()
     pass
 
