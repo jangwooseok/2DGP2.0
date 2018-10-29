3a02e2f9ad54ca88553851ee669e1a99726d6f40
장우석 <cyj2651@naver.com>
2018-10-29 21:58:24 +0900

    수면상태로 변환
    시간이 지나면 수면상태로 변환

--- Drill_12/boy.py
+++ Drill_12/boy.py
@@ -46,7 +46,7 @@ class IdleState:
             boy.velocity -= RUN_SPEED_PPS
         elif event == LEFT_UP:
             boy.velocity += RUN_SPEED_PPS
-        boy.timer = 1000
+        boy.timer = get_time()
 
     @staticmethod
     def exit(boy, event):
@@ -57,8 +57,8 @@ class IdleState:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        boy.timer -= 1
-        if boy.timer == 0:
+        #boy.timer -= 1
+        if get_time() - boy.timer >= 3 - 1:
             boy.add_event(SLEEP_TIMER)
 
     @staticmethod
